1e0402f106c60151605716f57b94e14e0cdb580a
Varshith R V <vrv278@gmail.com>
2024-06-27 20:42:16 +0800

    Initial_commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/barracuda/catkin_ws/src/mmsg_sdk/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/barracuda/catkin_ws/src/mmsg_sdk/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/barracuda/catkin_ws/src/mmsg_sdk/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/barracuda/catkin_ws/src/mmsg_sdk/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/barracuda/catkin_ws/src/mmsg_sdk/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/barracuda/catkin_ws/src/mmsg_sdk:/home/barracuda/catkin_ws/src:/opt/ros/noetic/share'
---
+++ build/catkin_generated/stamps/mmsg_sdk/rosservice_wrapper.py.stamp
@@ -0,0 +1,23 @@
+import rospy
+from std_msgs.msg import Bool
+from std_srvs.srv import SetBool, SetBoolResponse, SetBoolRequest
+
+class RosServiceWrapper:
+    def __init__(self):
+        rospy.init_node('ros_service_wrapper')
+        self.service = rospy.Service('/gripper', SetBool, self.callback)
+        self.setpoint_pub = rospy.Publisher('/gripper_setpoint', Bool, queue_size=1)
+        self.setpoint_pub_timer = rospy.Timer(rospy.Duration(0.05), self.setpoint_pub_callback)
+        self.setpoint = False
+
+    def callback(self, req):
+        self.setpoint = req.data
+        return SetBoolResponse(success=True)
+    
+    def setpoint_pub_callback(self, event):
+        self.setpoint_pub.publish(Bool(data=self.setpoint))
+
+if __name__ == "__main__":
+    RosServiceWrapper()
+    rospy.loginfo("ROS Service Wrapper for gripper started")
+    rospy.spin()
